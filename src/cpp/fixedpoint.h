f39c7cf61b78ebc634aa383a2f139fd75e0c1a10
arkainoh <arkainoh@gmail.com>
2017-10-30 17:19:11 +0900

    write in c++ style (oop)

---
+++ src/cpp/fixedpoint.h
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <ieee754.h>
+#define SIGN 1
+#define EXPONENT 8
+#define MANTISSA 23
+#define EXP_BIAS 127
+
+using namespace std;
+
+typedef short fix16;
+typedef char fix8;
+
+void print_binary(int num, int len);
+
+template <typename T>
+class FixedPoint {
+	public:
+	T f;
+	int wl;
+	int iwl;
+	int fwl;
+	
+	FixedPoint(int wl, int iwl) {
+		this->wl = wl;
+		this->iwl = iwl;
+		this->fwl = wl - 1 - iwl;
+	}
+
+	FixedPoint(float f, int wl, int iwl) {
+		this->wl = wl;
+		this->iwl = iwl;
+		this->fwl = wl - 1 - iwl;
+		fix(f);
+	}
+
+	T fix(float f) {
+		T ret = 0;
+		ieee754_float standard;
+		standard.f = f;
+
+		unsigned int mantissa = standard.ieee.mantissa;
+		int exp = standard.ieee.exponent - EXP_BIAS;
+		unsigned int integer, fraction;
+
+		if(exp > 0) {
+			integer = mantissa >> MANTISSA - exp;
+			integer = integer | (1 << exp);
+
+			fraction = mantissa << SIGN + EXPONENT + exp;
+			fraction = fraction >> sizeof(unsigned int) * 8 - fwl;
+			integer = integer << fwl;
+
+			ret = ret | integer;
+
+		} else {
+			integer = 0;
+			fraction = mantissa | (1 << MANTISSA);
+			fraction = fraction >> MANTISSA - fwl - exp;
+		}
+
+		ret = ret | fraction;
+
+		if(standard.ieee.negative) {
+			ret = ~ret + 1;
+		}
+		this->f = ret;
+		return ret;
+	}
+	
+	void printb() {
+		int integer = this->f >> fwl;
+	
+		print_binary(integer, iwl + 1);
+		cout << ".";
+		print_binary(this->f, fwl);
+	}
+	
+	void printd() {
+		int num = this->f;
+		if(num >> this->wl - 1) {
+			cout << "-";
+			num = ~(num - 1);
+		}
+
+		int integer = num >> this->fwl;
+		float fraction = 0.0;
+		float div = 0.5;
+		unsigned int mask = 1 << this->fwl - 1;
+		for(int i = 0; i < this->fwl; i++) {
+			if(mask & num) {
+				fraction += div;
+			}
+			mask = mask >> 1;
+			div /= 2;
+		}
+		cout << (integer + fraction);
+	}
+
+	FixedPoint<T> operator+(FixedPoint<T> f) {
+		FixedPoint<T> ret(this->wl, this->iwl);
+		ret.f = this->f + f.f;
+		return ret;
+	}
+
+	FixedPoint<T> operator-(FixedPoint<T> f) {
+		FixedPoint<T> ret(this->wl, this->iwl);
+		ret.f = this->f - f.f;
+		return ret;
+	}
+	
+	FixedPoint<T> operator*(FixedPoint<T> f) {
+		FixedPoint<T> ret(this->wl, this->iwl);
+		int tmp = this->f * f.f;
+		ret.f = tmp >> this->fwl;
+		return ret;
+	}
+
+	FixedPoint<T> operator/(FixedPoint<T> f) {
+		FixedPoint<T> ret(this->wl, this->iwl);
+		int tmp = this->f << this->fwl;
+		ret.f = tmp / f.f;
+		return ret;
+	}
+
+};
+
+void print_binary(int num, int len) {
+	
+	unsigned int mask = 1 << len - 1;
+
+	while(len--) {
+		if(num & mask) cout << "1";
+		else cout << "0";
+
+		mask = mask >> 1;
+	}
+}
+
---
+++ src/cpp/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "fixedpoint.h"
+
+int main() {
+	ieee754_float a;
+
+	cin >> a.f;
+
+	cout << "sign: ";
+	print_binary(a.ieee.negative, 1);
+	cout << endl;
+
+	cout << "exponent: ";
+	print_binary(a.ieee.exponent, 8);
+	cout << endl;
+
+	cout << "mantissa: ";
+	print_binary(a.ieee.mantissa, 23);
+	cout << endl;
+
+	FixedPoint<fix16> f16(16, 3);
+	f16.fix(a.f);
+	cout << endl << "converted to fix16" << endl;
+	f16.printb();
+	
+	cout << " (";
+	print_binary(f16.f, 16);
+	cout << ")" << endl;
+
+	f16.printd();
+	cout << endl;
+
+	FixedPoint<fix8> f8(8, 3);
+	f8.fix(a.f);
+	cout << endl << "converted to fix8" << endl;
+	f8.printb();
+
+	cout << " (";
+	print_binary(f8.f, 8);
+	cout << ")" << endl;
+
+	f8.printd();
+	cout << endl;
+	return 0;
+}
+
